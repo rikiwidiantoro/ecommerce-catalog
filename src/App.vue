5c954846d7d31d45337295a0193c991faec6b6c7
Riki Widiantoro <rikitoro22@gmail.com>
2022-12-27 11:03:18 +0700

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ProductDisplay/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ProductDisplay from './components/ProductDisplay.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    ProductDisplay
   }
 }
 </script>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="display-product">
+    
+    <div class="card">
+      <div class="gambar">
+        <img src="" alt="Gambar">
+      </div>
+      <div class="container">
+        <h4><b>John Doe</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .display-product {
+    margin: 100px auto;
+    width: 1000px;
+    height: 500px;
+  }
+  .card {
+    /* Add shadows to create the "card" effect */
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    display: flex;
+  }
+
+  .gambar {
+    flex: 1;
+    width: 20%;
+    padding: 2px 16px;
+  }
+
+  /* On mouse-over, add a deeper shadow */
+  .card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+
+  /* Add some padding inside the card container */
+  .container {
+    flex: 2;
+    padding: 2px 16px;
+    width: 80%;
+  }
+</style>
